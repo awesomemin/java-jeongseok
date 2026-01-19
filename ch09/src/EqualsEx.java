502e19c00c4a2dfd155be24b7fd627ecfc6ad572
Seongmin Oh <saogig0503@naver.com>
2026-01-19 13:20:54 +0900

    chapter 9

---
+++ ch09/src/EqualsEx.java
@@ -0,0 +1,33 @@
+class EqualsEx {
+    public static void main(String[] args) {
+        Value v1 = new Value(10);
+        Value v2 = new Value(10);
+
+        if(v1.equals(v2))
+            System.out.println("v1 equals to v2");
+        else
+            System.out.println("v1 NOT equals to v2");
+
+        v2 = v1;
+
+        if(v1.equals(v2))
+            System.out.println("v1 equals to v2");
+        else
+            System.out.println("v1 NOT equals to v2");
+    }
+}
+
+class Value {
+    int value;
+
+    Value(int value) {
+        this.value = value;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj instanceof Value v) {
+            return v.value == value;
+        }
+        return false;
+    }
+}
---
+++ ch09/src/WrapperEx.java
@@ -0,0 +1,7 @@
+class WrapperEx {
+    public static void main(String[] args) {
+        Integer i = Integer.valueOf(100);
+        Integer i2 = Integer.valueOf("100");
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
